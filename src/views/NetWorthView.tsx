685ea6238b467370bd9a39ff07b379d3577c1e8f
Tony <tonyyu1129@gmail.com>
2024-02-19 17:21:13 -0800

    installed react navigation

---
+++ src/views/NetWorthView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function NetWorthView() {
+  return (
+    <SafeAreaView>
+      <Text>Net Worth</Text>
+    </SafeAreaView>
+  );
+}
