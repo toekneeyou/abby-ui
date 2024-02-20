b91f7cfcf93af81ec560a22a5bb4d42e49418af6
Tony <tonyyu1129@gmail.com>
2024-02-20 14:42:18 -0800

    rough draft of account cards

---
+++ src/features/AccountCard.tsx
@@ -0,0 +1,158 @@
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import React, {useState} from 'react';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {colors} from '../styles/styleVariables';
+import Sofi from '../../assets/images/sofi.jpeg';
+import IconButton from '../components/IconButton';
+import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons';
+import {typography} from '../styles/globalStyles';
+
+type AccountCardItem = {
+  category: string;
+  value: string;
+  icon: IconProp;
+  accounts: AccountCardAccount[];
+};
+
+type AccountCardAccount = {
+  name: string;
+  type: string;
+  value: string;
+  syncTime: string;
+};
+
+type AccountCardHeaderProps = {
+  item: AccountCardItem;
+  isCollapsed: boolean;
+  setIsCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function AccountCardHeader({
+  item,
+  isCollapsed,
+  setIsCollapsed,
+}: AccountCardHeaderProps) {
+  return (
+    <Pressable
+      onPress={() => setIsCollapsed(!isCollapsed)}
+      style={styles.header}>
+      <View style={styles.headerLeft}>
+        <FontAwesomeIcon icon={item.icon} color={colors.eggplant[50]} />
+        <Text style={[typography.b1, {fontWeight: 'bold'}]}>
+          {item.category}
+        </Text>
+      </View>
+      <View style={styles.headerRight}>
+        <Text style={[typography.b1, {fontWeight: 'bold'}]}>{item.value}</Text>
+        <FontAwesomeIcon
+          icon={isCollapsed ? faChevronDown : faChevronUp}
+          size={16}
+        />
+      </View>
+    </Pressable>
+  );
+}
+
+type AccountCardItemProps = {
+  account: AccountCardAccount;
+  isLast: boolean;
+};
+
+function AccountCardItem({account, isLast}: AccountCardItemProps) {
+  return (
+    <View style={[styles.item, isLast && styles.itemLast]}>
+      <View style={styles.itemLeft}>
+        <View>
+          <Image
+            source={Sofi}
+            style={{width: 16, height: 16, borderRadius: 100}}
+          />
+        </View>
+        <View style={styles.itemLeftInfo}>
+          <Text style={typography.b1}>{account.name}</Text>
+          <Text style={[typography.b2, {color: colors.black[0]}]}>
+            {account.type}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.itemRight}>
+        <Text style={typography.b1}>{account.value}</Text>
+        <Text style={[typography.b2, {color: colors.black[0]}]}>
+          {account.syncTime}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+type AccountCardProps = {
+  item: AccountCardItem;
+};
+
+export default function AccountCard({item}: AccountCardProps) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  return (
+    <View style={styles.container}>
+      <AccountCardHeader
+        item={item}
+        isCollapsed={isCollapsed}
+        setIsCollapsed={setIsCollapsed}
+        g
+      />
+      {!isCollapsed &&
+        item.accounts.map((account, index) => {
+          const isLast = index === item.accounts.length - 1;
+          return (
+            <AccountCardItem
+              account={account}
+              key={`${account.name}${account.value}`}
+              isLast={isLast}
+            />
+          );
+        })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  header: {
+    height: 65,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 5,
+  },
+  headerRight: {flexDirection: 'row', alignItems: 'center', columnGap: 5},
+  item: {
+    backgroundColor: colors.gray[0],
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray[10],
+    padding: 10,
+    height: 65,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  itemLast: {
+    borderBottomWidth: 0,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    columnGap: 5,
+  },
+  itemLeftInfo: {},
+  itemRight: {alignItems: 'flex-end'},
+});
--- src/screens/NetWorthScreen.tsx
+++ src/screens/NetWorthScreen.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 
-import {typography} from '../styles/globalStyles';
 import {RootStackParamList} from '../../App';
 import InfoDisplay from '../features/InfoDisplay';
 import {colors} from '../styles/styleVariables';
+import AccountCard from '../features/AccountCard';
+import {
+  faArrowTrendUp,
+  faCreditCard,
+  faSackDollar,
+} from '@fortawesome/free-solid-svg-icons';
 
 type NetWorthScreenProps = BottomTabScreenProps<
   RootStackParamList,
@@ -18,42 +23,107 @@ export default function NetWorthScreen({navigation}: NetWorthScreenProps) {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <InfoDisplay />
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
-        <Text style={typography.h1}>Net Worth</Text>
+        <View style={styles.accountCardsContainer}>
+          <AccountCard
+            item={{
+              category: 'Cash',
+              icon: faSackDollar,
+              value: '$1,234',
+              accounts: [
+                {
+                  name: 'SoFi',
+                  type: 'Checking',
+                  value: '$100.23',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'SoFi',
+                  type: 'Savings',
+                  value: '$1,200.55',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'Chase',
+                  type: 'Checking',
+                  value: '$1,211.41',
+                  syncTime: '2 minutes ago',
+                },
+                {
+                  name: 'Ally',
+                  type: 'Checking',
+                  value: '$1.89',
+                  syncTime: '3 minutes ago',
+                },
+              ],
+            }}
+          />
+          <AccountCard
+            item={{
+              category: 'Investments',
+              icon: faArrowTrendUp,
+              value: '$3,212',
+              accounts: [
+                {
+                  name: 'RobinHood',
+                  type: 'Brokerage',
+                  value: '$100.23',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'SoFi',
+                  type: 'Brokerage',
+                  value: '$1,200.55',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'WeBull',
+                  type: 'Brokerage',
+                  value: '$1,200.55',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'Fidelity',
+                  type: 'Brokerage',
+                  value: '$1,200.55',
+                  syncTime: '1 minute ago',
+                },
+              ],
+            }}
+          />
+          <AccountCard
+            item={{
+              category: 'Credit Cards',
+              icon: faCreditCard,
+              value: '$56,231',
+              accounts: [
+                {
+                  name: 'SoFi',
+                  type: 'Credit Card',
+                  value: '$100.23',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'Citi',
+                  type: 'Credit Card',
+                  value: '$1,200.55',
+                  syncTime: '1 minute ago',
+                },
+                {
+                  name: 'Chase',
+                  type: 'Credit Card',
+                  value: '$1,211.41',
+                  syncTime: '2 minutes ago',
+                },
+                {
+                  name: 'Wells Fargo',
+                  type: 'Credit Card',
+                  value: '$1.89',
+                  syncTime: '3 minutes ago',
+                },
+              ],
+            }}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -62,6 +132,11 @@ export default function NetWorthScreen({navigation}: NetWorthScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.white,
+    backgroundColor: colors.eggplant[30],
+  },
+  accountCardsContainer: {
+    backgroundColor: colors.eggplant[30],
+    padding: 15,
+    rowGap: 15,
   },
 });
