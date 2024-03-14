9edb3443751c0e1f3cbf04f49156ef79265ec4f6
Tony <tonyyu1129@gmail.com>
2024-03-13 19:20:09 -0700

    set u react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(false);
+  const presets = ['module:@react-native/babel-preset'];
+  const plugins = [['module:react-native-dotenv']];
+
+  return {presets, plugins};
 };
