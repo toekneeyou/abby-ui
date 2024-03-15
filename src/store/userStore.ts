8ba2f0be49eae07229190b0929d05474ea51ee05
Tony <tonyyu1129@gmail.com>
2024-03-14 18:43:21 -0700

    add user to redux store

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -2,16 +2,34 @@ import {createSlice} from '@reduxjs/toolkit';
 import type {PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from './store';
 
-interface UserState {}
+export type User = {
+  username?: string;
+  password?: string;
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  salt?: string;
+};
 
-export const initialState: UserState = {};
+interface UserState {
+  user: User | undefined;
+}
+
+export const initialState: UserState = {user: undefined};
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const {setUser} = userSlice.actions;
+
+export const getUser = (state: RootState) => state.user.user;
 
 export default userSlice.reducer;
