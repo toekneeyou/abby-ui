f3ae0844bc72bc88bcbda28e707b77c604a1f6fd
Tony <tonyyu1129@gmail.com>
2024-03-14 18:39:55 -0700

    add path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
 module.exports = function (api) {
   api.cache(false);
   const presets = ['module:@react-native/babel-preset'];
-  const plugins = [['module:react-native-dotenv']];
+  const plugins = [
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@features': './src/features',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@styles': './src/styles',
+          '@types': './src/types',
+        },
+      },
+    ],
+  ];
 
   return {presets, plugins};
 };
