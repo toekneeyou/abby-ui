be4ab930c377f48aac06063dbc521ec7e4849342
Tony <tonyyu1129@gmail.com>
2024-03-20 16:47:58 -0700

    add helpers

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -1,3 +1,24 @@
+import {Account, AccountsState} from '@store/financialDataStore';
+
 export const isDev = () => {
   return process.env.NODE_ENV === 'development';
+};
+
+export const organizeAccountsResponseByCategory = (accounts: Account[][]) => {
+  const organizedAcounts: AccountsState = {};
+  accounts.flat().forEach(account => {
+    if (organizedAcounts[account.plaidType]) {
+      organizedAcounts[account.plaidType]?.push(account);
+    } else {
+      organizedAcounts[account.plaidType] = [account];
+    }
+  });
+  return organizedAcounts;
+};
+
+export const returnCurrency = (decimal: number) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(decimal);
 };
