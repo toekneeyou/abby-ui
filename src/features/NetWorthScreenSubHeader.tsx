594bf2d3062686c0785b6bb730b386478d09cbdd
Tony <tonyyu1129@gmail.com>
2024-03-22 18:07:41 -0700

    use selectedNetworth in subheader

--- src/features/NetWorthScreenSubHeader.tsx
+++ src/features/NetWorthScreenSubHeader.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import {typography} from '../styles/globalStyles';
+import {useAppSelector} from '@store/store';
+import {getSelectedNetWorth} from '@store/financialDataStore';
+import {returnCurrency} from '@services/helper';
 
 export default function NetWorthSubHeader() {
+  const selectedNetWorth = useAppSelector(getSelectedNetWorth);
+
   return (
     <View style={styles.netWorthSubHeader}>
       <View style={styles.netWorthSubHeaderLeft}>
         <Text style={typography.h3}>Net Worth</Text>
       </View>
       <View style={styles.netWorthSubHeaderRight}>
-        <Text style={typography.h3}>$1,234.23</Text>
+        <Text style={typography.h3}>
+          {selectedNetWorth
+            ? returnCurrency(selectedNetWorth.amount as number)
+            : '$0.00'}
+        </Text>
       </View>
     </View>
   );
